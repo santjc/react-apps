8ead0aee240a35747db7dc706f840b99fb6e1644
Santiago Colombatto <sjcolombatto@hotmail.com>
2021-02-09 11:37:40 -0300

    Img

--- github-users-app/src/components/RepoGrid.js
+++ github-users-app/src/components/RepoGrid.js
@@ -9,18 +9,25 @@ export default class RepoGrid extends React.Component {
     return (
       <div className="container-xl">
         <ul className="grid-container">
-          {this.props.repos.map(({ name, owner, stargazers_count, html_url }) => (
-            <li key={name} className="grid-item">
-              <div className="profile-pic"></div>
-              <ul className="repo-data">
-                <li>
-                  <a href={html_url}>{name}</a>
-                </li>
-                <li>@{owner.login}</li>
-                <li>{stargazers_count} stars</li>
-              </ul>
-            </li>
-          ))}
+          {this.props.repos.map(
+            ({ name, owner, stargazers_count, html_url }) => (
+              <li key={name} className="grid-item">
+                <div
+                  className="profile-pic"
+                  style={{
+                    background: `url(https://github.com/${owner.login}.png)`,
+                  }}
+                ></div>
+                <ul className="repo-data">
+                  <li>
+                    <a href={html_url}>{name}</a>
+                  </li>
+                  <li>@{owner.login}</li>
+                  <li>{stargazers_count} stars</li>
+                </ul>
+              </li>
+            )
+          )}
         </ul>
       </div>
     );
