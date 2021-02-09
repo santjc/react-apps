017a3853cf18a046a2748eb0a5b895ed4476cc0a
Santiago Colombatto <sjcolombatto@hotmail.com>
2021-02-09 18:42:55 -0300

    Search bar

--- github-users-app/src/components/Navbar.js
+++ github-users-app/src/components/Navbar.js
@@ -3,6 +3,19 @@ import React from "react";
 export default class Navbar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { inputValue: "" };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log(this.props);
+    event.preventDefault();
+    this.props.onSelectLanguage(this.state.inputValue);
   }
   render() {
     const languages = ["all", "javascript", "ruby", "python"];
@@ -20,6 +33,17 @@ export default class Navbar extends React.Component {
               </li>
             ))}
           </ul>
+          <form onSubmit={this.handleSubmit} className="search-form">
+              <input
+                type="text"
+                value={this.state.inputValue}
+                onChange={this.handleInputChange}
+                placeholder="Search a language"
+              />
+              <button class="submit-btn" type="submit">
+                <i class="fas fa-search"></i>
+              </button>
+            </form>
         </div>
       </nav>
     );
